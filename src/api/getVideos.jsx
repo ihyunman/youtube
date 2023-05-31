3e6fbf32c9737ba22a137c8e4699ccdd74ff76ea
ihyunman <ihyunman94@gmail.com>
2023-05-31 15:34:21 +0900

    change axios url

--- src/api/getVideos.jsx
+++ src/api/getVideos.jsx
@@ -3,7 +3,9 @@ import axios from "axios";
 export default async function getVideos({ queryKey }) {
   const type = queryKey[1];
   // get url에 /로 시작하지않으면 현재 경로를 기준으로 작동한다.
-  return await axios.get(`/items/${type}.json`).then((res) => res.data.items);
+  return await axios
+    .get(`https://ihyunman.github.io/youtube/items/${type}.json`)
+    .then((res) => res.data.items);
   // console.log("fetch");
   // return axios.get(`videos/popular.json`).then((res) => res.data.items);
 }
